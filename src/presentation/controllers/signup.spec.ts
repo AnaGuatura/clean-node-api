995db33b9197727ebed2cd8e3299cdb44bca0dce
Ana <ana_claraguatura@hotmail.com>
2020-09-24 10:06:19 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,19 @@
+// rota que vamos passar nome, email e senha para criar a conta de um usuário
+
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        // name: 'any_name', removendo o name para validar o teste
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_passwrowd'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
