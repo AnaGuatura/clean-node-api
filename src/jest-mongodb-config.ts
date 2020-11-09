aff2a169203a47ec3b9681273dab875cbe3cfa28
Ana <ana_claraguatura@hotmail.com>
2020-11-09 10:02:24 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/jest-mongodb-config.ts
+++ src/jest-mongodb-config.ts
@@ -1,12 +1,15 @@
 module.exports = {
   mongodbMemoryServerOptions: {
     binary: {
-      version: '4.0.3',
+      version: '3.6.20',
       skipMD5: true
     },
     instance: {
       dbName: 'jest'
     },
     autoStart: false
+  },
+  mongodbMemoryServer: {
+    version: 'latest'
   }
 }
